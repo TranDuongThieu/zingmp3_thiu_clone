ea4b9e9658e5e80f04b9fa3df7a9eff7b6aa99d2
TranDuongThieu <thieu098dm3@gmail.com>
2023-03-04 18:27:47 +0700

    my_project

---
+++ src/store/actions/search.js
@@ -0,0 +1,27 @@
+import {  apiSearch } from "../../api";
+import actionTypes from "./actionTypes";
+
+// export const search = (keyword) => async (dispatch) => {
+//     try {
+//         const res = await apiSearch(keyword);
+//         if (res.data.err === 0) {
+//             dispatch({
+//                 type: actionTypes.SEARCH,
+//                 data: res.data.data,
+//             });
+//         } else
+//             dispatch({
+//                 type: actionTypes.SEARCH,
+//                 data: null,
+//             });
+//     } catch (error) {
+//         dispatch({
+//             type: actionTypes.SEARCH,
+//             data: null,
+//         });
+//     }
+// };
+export const searchID = (id)=>({
+    type: actionTypes.SET_SEARCH_ID,
+    id
+})
